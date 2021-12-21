f2a62c98bc3a367960f53658ee422df69b0fef13
Mayur-Arde <mayurarde8@gmail.com>
2021-12-21 09:41:17 +0530

    img added

--- js/script.js
+++ js/script.js
@@ -10,3 +10,15 @@ navToggle.addEventListener("click", () => {
     icon.classList.toggle("hidden");
   });
 });
+
+//roman to integer
+function romanToInt(str) {
+  var roman = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
+  var int = 0;
+  for (var i of str) {
+    int += roman[i];
+  }
+  return int;
+}
+
+
